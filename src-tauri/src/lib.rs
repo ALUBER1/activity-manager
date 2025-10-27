8b22be0ab9532d7b1df3572c4eb4e2626a2d2b88
ALUBER1 <yfilizola@gmail.com>
2025-10-27 20:39:05 +0100

    tray might work on linux and macos

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -36,48 +36,45 @@ fn maximize_app(app_handle: AppHandle) {
 
 #[tauri::command]
 fn tray_app(app_handle: AppHandle) {
-    #[cfg_attr(all(target_os = "windows",not(debug_assertions)), windows_subsystem = "windows")]
-    {
-        if let Some(window) = app_handle.get_webview_window("main") {
-            let _ = window.hide();
-        }
-        let close = MenuItem::with_id(&app_handle, "close", "close", true, None::<&str>).unwrap();
-
-        let menu = Menu::with_items(&app_handle, &[&close]).unwrap();
-
-        let cloned = app_handle.clone();
-        let _ = TrayIconBuilder::with_id("tray")
-            .menu(&menu)
-            .show_menu_on_left_click(false)
-            .on_menu_event(move |_tray, event| match event.id.as_ref() {
-                "close" => {
-                    close_app(cloned.clone());
-                },
-                _ => {
-
-                }
-            })
-            .on_tray_icon_event(|tray: &TrayIcon, event| match event {
-                TrayIconEvent::Click { 
-                    id: _, 
-                    position: _, 
-                    rect: _, 
-                    button: MouseButton::Left, 
-                    button_state: MouseButtonState::Up
-                } => {
-                    let app = tray.app_handle();
-                    if let Some(window) = app.get_webview_window("main") {
-                        let _ = window.show();
-                        let _ = window.set_focus();
-                        let _ = tray.set_visible(false);
-                    }
-                },
-                _ => {}
-            })
-            .icon(app_handle.default_window_icon().unwrap().clone())
-            .build(&app_handle)
-            .unwrap();
+    if let Some(window) = app_handle.get_webview_window("main") {
+        let _ = window.hide();
     }
+    let close = MenuItem::with_id(&app_handle, "close", "close", true, None::<&str>).unwrap();
+
+    let menu = Menu::with_items(&app_handle, &[&close]).unwrap();
+
+    let cloned = app_handle.clone();
+    let _ = TrayIconBuilder::with_id("tray")
+        .menu(&menu)
+        .show_menu_on_left_click(false)
+        .on_menu_event(move |_tray, event| match event.id.as_ref() {
+            "close" => {
+                close_app(cloned.clone());
+            },
+            _ => {
+
+            }
+        })
+        .on_tray_icon_event(|tray: &TrayIcon, event| match event {
+            TrayIconEvent::Click { 
+                id: _, 
+                position: _, 
+                rect: _, 
+                button: MouseButton::Left, 
+                button_state: MouseButtonState::Up
+            } => {
+                let app = tray.app_handle();
+                if let Some(window) = app.get_webview_window("main") {
+                    let _ = window.show();
+                    let _ = window.set_focus();
+                    let _ = tray.set_visible(false);
+                }
+            },
+            _ => {}
+        })
+        .icon(app_handle.default_window_icon().unwrap().clone())
+        .build(&app_handle)
+        .unwrap();
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
